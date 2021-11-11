9a00cd6b4d78a50c62924c163ce0df235352d0a6
alejandro <ftvalex@hotmail.com>
2021-11-11 10:37:22 +0000

    Primer reducer (DUCK)
    Usando la metodologia DUCK para crear nuestro primer reducer.

---
+++ src/redux/pokeDucks.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+
+
+//constantes
+const dataInicial = [
+    array : []
+]
+//Types
+const OBTENER_POKEMONES_EXITO = "OBTENER_POKEMONES_EXITO"
+
+
+//reducer
+export default function pokeReducer(state = dataInicial, action){
+    switch(action.type){
+        case OBTENER_POKEMONES_EXITO:
+            return {
+                ...state , array: action.payload
+            }
+            default:
+                return state
+    }
+}
+
+//acciones
+
+export function obtenerPokemonesAccion = () => async(dispatch,getState) =>{
+    try {
+        const res = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+        dispatch({
+            type: OBTENER_POKEMONES_EXITO,
+            payload: res.data.results
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+//en la primera funcion recibimos los parametros
+//en la segunda funcion necesita dispatch y getState , con dispatch activamos el reducir  y con el getState recibimos la data inicial
