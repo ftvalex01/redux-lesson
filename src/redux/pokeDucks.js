b32ea7ef20e2b9f46b19da2bdcd114ad0adb6e6c
alejandro <ftvalex@hotmail.com>
2021-11-15 09:18:50 +0000

    Guardando datos en La localstorage
    guardando datos en localstorage para no hacer tantas llamadas a la api

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -42,7 +42,13 @@ export default function pokeReducer(state = dataInicial, action){
 export const obtenerPokemonesAccion = () => async (dispatch,getState) =>{
 
    //const offset =  getState().pokemones.offset
-
+    if(localStorage.getItem('offset=0')){
+        dispatch({
+            type: OBTENER_POKEMONES_EXITO,
+            payload: JSON.parse(localStorage.getItem('offset=0'))
+        })
+        return
+    }
 
     try {
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
@@ -50,6 +56,7 @@ export const obtenerPokemonesAccion = () => async (dispatch,getState) =>{
             type: OBTENER_POKEMONES_EXITO,
             payload: res.data
         })
+        localStorage.setItem('offset=0',JSON.stringify(res.data))
     } catch (error) {
         console.log(error)
     }
@@ -63,13 +70,23 @@ export const obtenerSiguientePagina = () => async(dispatch,getState) =>{
    /*  const offset =  getState().pokemones.offset
     const siguiente = offset + numero */
 
+    if(localStorage.getItem(next)){
+        console.log("datos guardados")
+        dispatch({
+            type: SIGUIENTE_POKEMONES_EXITO,
+            payload: JSON.parse(localStorage.getItem(next))
+        })
+        return
+    }
 
     try {
+        console.log("llamada api")
         const res = await axios.get(next)
         dispatch({
             type: SIGUIENTE_POKEMONES_EXITO,
             payload: res.data
         })
+        localStorage.setItem(next,JSON.stringify(res.data))
     } catch (error) {
         console.log(error)
     }
@@ -79,12 +96,24 @@ export const obtenerSiguientePagina = () => async(dispatch,getState) =>{
 export const obtenerAnteriorPagina = () =>async(dispatch,getState) =>{
     const previous = getState().pokemones.previous
 
+
+    if(localStorage.getItem(previous)){
+        console.log("datos guardados")
+        dispatch({
+            type: SIGUIENTE_POKEMONES_EXITO,
+            payload: JSON.parse(localStorage.getItem(previous))
+        })
+        return
+    }
+
+
     try {
         const res = await axios.get(previous)
         dispatch({
             type: ANTERIOR_POKEMONES_EXITO,
             payload:res.data
         })
+        localStorage.setItem(previous,JSON.stringify(res.data))
     } catch (error) {
         console.log(error)
     }
