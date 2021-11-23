ce525150880edf9c6fc6e281ea0bec77cd39470e
alejandro <ftvalex@hotmail.com>
2021-11-23 08:58:47 +0000

    Rutas protegidas con google auth
    trabajando con las rutas protegidas.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,60 @@ import Pokemones from "./components/Pokemones";
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom'
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import { auth } from "./firebase";
 
 
 function App() {
 
+  const [firebaseUser,setFirebaseUser] = React.useState(false)
+  
+  
+  React.useEffect(()=>{
+    const fetchUser = () =>{
+      auth.onAuthStateChanged(user=>{
+        if(user){
+          setFirebaseUser(user)
+        }else{
+          setFirebaseUser(null)
+        }
+      })
+  }
+  fetchUser()
+  
+  },[])
 
-  return (
+
+  const RutaPrivada = ({component,path,...rest}) =>{
+    if(localStorage.getItem('usuario')){
+      const usuarioStorage = JSON.parse(localStorage.getItem('usuario'))
+      if(usuarioStorage.uid === firebaseUser.uid){
+        return <Route component={component} path={path} {...rest}/>
+      }else{
+        return <Redirect to="/login" {...rest}/>
+      }
+    }else{
+      return <Redirect to="/login"{...rest}/>
+    }
+  }
+
+
+
+  return firebaseUser !== false ?(
     <Router>
       <div className="container mt-3">
       <Navbar/>
         <Switch>
-          <Route component={Pokemones} path="/" exact/>
+          <RutaPrivada component={Pokemones} path="/" exact/>
           <Route component={Login} path="/login" exact/>
         </Switch>
       </div>
     </Router>
-
-
-
-
-  );
+  ) : (<div>Cargando....</div>)
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import {Link,NavLink} from 'react-router-dom'
-import {useDispatch} from 'react-redux'
+import {useDispatch,useSelector} from 'react-redux'
 import { cerrarSesion } from '../redux/usuarioDucks'
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router-dom'
 
 
 
@@ -15,14 +15,24 @@ const Navbar = (props) => {
         props.history.push("/login")
     }
 
+    const activo = useSelector(store=>store.usuario.activo)
+
 
     return (
         <div className="navbar navbar-dark bg-dark">
             <Link className="navbar-brand" to="/">APP POKE</Link>
             <div className="d-flex">
-                <NavLink className="btn btn-dark mr-2" to="/" exact>Inicio</NavLink>
-                <NavLink className="btn btn-dark mr-2" to="/login" exact>login</NavLink>
-                <button className="btn btn-dark mr-2" onClick={()=>(cerrarSesionActiva())}>cerrar sesion</button>
+            {
+                activo ?(
+                    <>
+                    <NavLink className="btn btn-dark mx-2" to="/" exact>Inicio</NavLink>
+                    <button className="btn btn-dark mx-2" onClick={()=>(cerrarSesionActiva())}>cerrar sesion</button>
+                    </>
+                ) : (
+                    <NavLink className="btn btn-dark mx-2" to="/login" exact>login</NavLink>
+                )
+            }
+      
             </div>
         </div>
     )
